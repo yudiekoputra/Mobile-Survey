c8ccf902d94517fe7bc9467beb75735caf3b0ee7
Yudi Eko Putra <yudie.1106@gmail.com>
2019-10-30 11:58:59 +0700

    API service

--- app/src/main/java/com/bcafinance/itdp/mobilesurvey/helper/RequestAPIServices.java
+++ app/src/main/java/com/bcafinance/itdp/mobilesurvey/helper/RequestAPIServices.java
@@ -1,4 +1,23 @@
 package com.bcafinance.itdp.mobilesurvey.helper;
 
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
 public interface RequestAPIServices {
+//contoh dari BCAITDP
+    //deklarasikan semua mthod yang berhubungan dengan API request
+
+    //untuk memanggil list user
+//    @GET("users")
+//    Call<ListUserModel> getListUser(@Query("page")int page);
+
+    //ini untuk create user baru
+//    @FormUrlEncoded
+//    @POST("users")
+//    Call<CreateUser> createUser(@Field("name") String name, @Field("job") String job);
+
 }
